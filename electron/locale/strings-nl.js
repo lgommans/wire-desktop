7f4e6a833b0100dbdffe21fb35d29130a97f0777
Luc Gommans <git-client@lgms.nl>
2017-02-22 11:47:32 +0100

    Added Dutch locale (nl)

---
+++ electron/locale/strings-nl.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let string = {};
+
+string.menuConversation = 'Gesprek';
+string.menuStart = 'Start';
+string.menuEdit = 'Bewerken';
+string.menuWindow = 'Venster';
+string.menuView = 'Weergave';
+string.menuHelp = 'Help';
+string.menuStartup = 'Start Wire bij het Opstarten';
+string.menuLocale = 'Taal';
+string.menuAbout = 'Over Wire';
+string.menuSignOut = 'Uitloggen';
+string.menuPing = 'Ping';
+string.menuCall = 'Bellen';
+string.menuVideoCall = 'Videobellen';
+string.menuPeople = 'Personen';
+string.menuAddPeople = 'Voeg personen toe...';
+string.menuArchive = 'Archiveer';
+string.menuUnarchive = 'Dearchiveren';
+string.menuMute = 'Demp';
+string.menuUnmute = 'Dempen uitzetten';
+string.menuLeave = 'Verlaat';
+string.menuDelete = 'Verwijder';
+string.menuBlock = 'Blokkeer';
+string.menuUndo = 'Ongedaan maken';
+string.menuRedo = 'Opnieuw doen';
+string.menuCut = 'Knippen';
+string.menuCopy = 'Kopiëren';
+string.menuPaste = 'Plakken';
+string.menuSelectAll = 'Alles Selecteren';
+string.menuSpelling = 'Controleer Spelling Tijdens het Typen';
+string.menuMinimize = 'Minimaliseren';
+string.menuClose = 'Sluiten';
+string.menuNextConversation = 'Volgend Gesprek';
+string.menuPreviousConversation = 'Vorig Gesprek';
+string.menuFullScreen = 'Volledig Scherm';
+string.menuLegal = 'Rechten';
+string.menuPrivacy = 'Privacy';
+string.menuLicense = 'Licentie-informatie';
+string.menuSupport = 'Ondersteuningswebsite';
+string.menuWireURL = 'Wire\'s Website';
+string.menuPreferences = 'Voorkeuren';
+string.menuServices = 'Diensten';
+string.menuHideWire = 'Verberg Wire';
+string.menuHideOthers = 'Verberg Overigen';
+string.menuShowAll = 'Toon Alles';
+string.menuSettings = 'Instellingen';
+string.menuQuit = 'Sluit Wire';
+string.menuShowHide = 'Toon/Verberg Menu';
+string.menuSavePictureAs = 'Afbeelding Opslaan Als...';
+string.menuNoSuggestions = 'Geen Suggesties';
+
+string.restartNeeded = 'Herstart Vereist';
+string.restartLocale = 'Herstart de applicatie om de instelling van kracht te laten worden.';
+string.restartLater = 'Later';
+string.restartNow = 'Nu Herstarten';
+
+string.trayOpen = 'Open';
+string.trayQuit = 'Sluit';
+
+string.unreadMessages = 'Ongelezen bericht';
+
+module.exports = string;
